ecac9898294e73b0cbed78e407fe58b367f7758d
Mohamed Zeina <toxiczeina@gmail.com>
2024-07-05 14:27:40 +0300

    added host name to the redis createClient call for the containers to communicate

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: 'redis-server', // Name of the container (service) in docker-compose.yml
+  port: 6379, // Default redis server port
+});
 
 client.set('visits', 0); // Initializing the number of visits on the redis server
 
